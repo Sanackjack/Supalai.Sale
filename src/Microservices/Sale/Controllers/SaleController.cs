a7191433e0a5bfe11be76fa85063d29bb658b32d
Supachai Sriphothong <nack.sriphothong@gmail.com>
2022-10-20 22:28:12 +0700

    create sale service application

---
+++ src/Microservices/Sale/Controllers/SaleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Supalai.Sale.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaleController : ControllerBase
+    {
+        [HttpGet("get/{id}", Name = "GetSaleById")]
+        public IActionResult GetSaleById([FromRoute][Required] string id)
+        {
+
+            //Example to use custom Exception
+            //throw new AuthenticateException(ResponseDatas.AUTHENTICATION_FAIL);
+            //throw new AuthenticateException(ResponseDatas.AUTHENTICATION_FAIL.Code, ResponseDatas.AUTHENTICATION_FAIL.Message, ResponseDatas.AUTHENTICATION_FAIL.HttpStatus);
+
+            return new OkObjectResult("Hello world");
+
+        }
+    }
+}
+
