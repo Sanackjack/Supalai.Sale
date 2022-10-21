72fb88b63a3d64ef1e9e7724df6f1d38a6b4c455
Supachai Sriphothong <nack.sriphothong@gmail.com>
2022-10-21 23:00:57 +0700

    change structure main start to use Program.cs and Startup.cs

--- src/Microservices/Sale/Controllers/SaleController.cs
+++ src/Microservices/Sale/Controllers/SaleController.cs
@@ -11,13 +11,7 @@ namespace Supalai.Sale.Controllers
         [HttpGet("get/{id}", Name = "GetSaleById")]
         public IActionResult GetSaleById([FromRoute][Required] string id)
         {
-
-            //Example to use custom Exception
-            //throw new AuthenticateException(ResponseDatas.AUTHENTICATION_FAIL);
-            //throw new AuthenticateException(ResponseDatas.AUTHENTICATION_FAIL.Code, ResponseDatas.AUTHENTICATION_FAIL.Message, ResponseDatas.AUTHENTICATION_FAIL.HttpStatus);
-
             return new OkObjectResult("Hello world");
-
         }
     }
 }
--- src/Microservices/Sale/Program.cs
+++ src/Microservices/Sale/Program.cs
@@ -1,26 +1,23 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+﻿using ClassifiedAds.Infrastructure.Logging;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+namespace Supalai.Sale
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseClassifiedAdsLogger(configuration =>
+                {
+                    return new LoggingOptions();
+                });
+    }
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
