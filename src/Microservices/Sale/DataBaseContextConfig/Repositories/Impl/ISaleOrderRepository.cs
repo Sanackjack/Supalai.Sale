f66c74703b1cf70c37f21dbb07cfa0b58210a82b
Supachai Sriphothong <nack.sriphothong@gmail.com>
2022-11-14 23:17:03 +0700

    [OSW-75] delete unused class

--- src/Microservices/Sale/DataBaseContextConfig/Repositories/Impl/ISaleOrderRepository.cs
+++
@@ -1,8 +0,0 @@
-using Spl.Crm.SaleOrder.DataBaseContextConfig.Models;
-
-namespace Spl.Crm.SaleOrder.DataBaseContextConfig.Repositories.Impl;
-
-public interface ISaleOrderRepository
-{
-    List<SysUserInfo>? FindSysUserInfoRawSqlByUserName(string userName);
-}
