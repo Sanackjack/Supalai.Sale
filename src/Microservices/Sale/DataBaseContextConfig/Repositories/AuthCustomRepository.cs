a95002a924866579e4a8ce3c7ee55ebca3e02fc4
Supachai Sriphothong <nack.sriphothong@gmail.com>
2022-11-15 20:42:11 +0700

    [OSW-75] change to prepare state parameter for prevent sql inject

--- src/Microservices/Sale/DataBaseContextConfig/Repositories/AuthCustomRepository.cs
+++ src/Microservices/Sale/DataBaseContextConfig/Repositories/AuthCustomRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Spl.Crm.SaleOrder.DataBaseContextConfig.Models;
 using Spl.Crm.SaleOrder.DataBaseContextConfig.Repositories.Impl;
@@ -15,8 +16,18 @@ public class AuthCustomRepository : IAuthCustomRepository
    
    public List<SysUserInfo>? FindSysUserInfoRawSqlByUserName(string userName)
    {
-      List<SysUserInfo>? sysUserInfos = _db.SysUserInfo.FromSqlRaw(
-            $"select SAU.UserId,SAU.Username,SAU.Password,SAU.FirstName,SAU.FirstName_EN,SAU.LastName,SAU.LastName_EN,SAU.Language, SAU.Email,SAUP.Gender,SAUP.Phone,SAUP.Mobile,SAU.IsDelete,SAU.IsSuperUser,SAU.isOutSource,SAB.BUCode,SAB.BUName,SAR.RoleName,SAR.isSystemRole from dbo.Sys_Admin_Users SAU left join dbo.Sys_Admin_UserProfile SAUP on SAU.UserId = SAUP.UserId left join dbo.Sys_Admin_UsersInBU SAUIB on SAUP.UserId = SAUIB.UserId left join dbo.Sys_Admin_Bu SAB on SAUIB.BUId = SAB.BUId left join dbo.Sys_Admin_Roles SAR on SAB.RoleId = SAR.RoleId where SAU.ADUser = 'Yes' and SAU.IsDelete = 0 and SAB.isDelete = 0 and SAR.isDelete = 0 and SAU.Username = '{userName}'").OrderByDescending(x=>x.UserId).ToList();
+      // prepare statement parameter prevent sql inject
+      var name = new SqlParameter("@Name", userName);
+
+      List<SysUserInfo>? sysUserInfos = _db.SysUserInfo.FromSqlRaw<SysUserInfo>(
+            @"select SAU.UserId,SAU.Username,SAU.Password,SAU.FirstName,SAU.FirstName_EN,SAU.LastName,SAU.LastName_EN,SAU.Language, SAU.Email,SAUP.Gender,SAUP.Phone,SAUP.Mobile,SAU.IsDelete,SAU.IsSuperUser,SAU.isOutSource,SAB.BUCode,SAB.BUName,SAR.RoleName,SAR.isSystemRole 
+            from dbo.Sys_Admin_Users SAU 
+            left join dbo.Sys_Admin_UserProfile SAUP on SAU.UserId = SAUP.UserId 
+            left join dbo.Sys_Admin_UsersInBU SAUIB on SAUP.UserId = SAUIB.UserId 
+            left join dbo.Sys_Admin_Bu SAB on SAUIB.BUId = SAB.BUId 
+            left join dbo.Sys_Admin_Roles SAR on SAB.RoleId = SAR.RoleId 
+            where SAU.ADUser = 'Yes' and SAU.IsDelete = 0 and SAB.isDelete = 0 and SAR.isDelete = 0 and SAU.Username = @Name",name)
+         .OrderByDescending(x=>x.UserId).ToList();
 
       return sysUserInfos.Count != 0 ? sysUserInfos : null;
 
