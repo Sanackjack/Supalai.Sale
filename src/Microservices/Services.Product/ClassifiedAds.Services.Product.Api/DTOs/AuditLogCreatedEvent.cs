e7cab976602b918f8eb08705e28e9c8210073d3b
Supachai Sriphothong <nack.sriphothong@gmail.com>
2022-11-15 22:47:27 +0700

    remove product project

--- src/Microservices/Services.Product/ClassifiedAds.Services.Product.Api/DTOs/AuditLogCreatedEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using ClassifiedAds.Services.Product.Entities;
-
-namespace ClassifiedAds.Services.Product.DTOs
-{
-    public class AuditLogCreatedEvent
-    {
-        public AuditLogEntry AuditLog { get; set; }
-    }
-}
